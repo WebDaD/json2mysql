55df1a5d216dac6647c07e274d7f1c057ddf21c6
Dominik Sigmund <dominik.sigmund@br.de>
2019-01-11 09:53:42 +0100

    Basic Table Setup Tests

--- index.js
+++ index.js
@@ -16,30 +16,32 @@ function JSON2MySQL (options) {
       const field = this.options.fields[j]
       fields += '`' + field.field + '` ' + field.type + ' NOT NULL, '
     }
-    if (this.options.fields.length < 1) {
+    if (this.options.fields.length > 1) {
       fields = fields.substr(0, fields.length - 2)
     }
     let indices = ''
-    for (let j = 0; j < this.options.indices.length; j++) {
-      indices += '`' + this.options.indices[j] + '`, '
+    if (this.options.indices && this.options.indices.length > 0) {
+      for (let j = 0; j < this.options.indices.length; j++) {
+        indices += '`' + this.options.indices[j] + '`, '
+      }
+      indices = ', INDEX (' + indices.substr(0, indices.length - 2) + ') '
     }
-    indices = ', INDEX (' + indices.substr(0, indices.length - 2) + ') '
-
     let primary = ''
-    for (let j = 0; j < this.options.primary.length; j++) {
-      primary += '`' + this.options.primary[j] + '`, '
+    if (this.options.primary && this.options.primary.length > 0) {
+      for (let j = 0; j < this.options.primary.length; j++) {
+        primary += '`' + this.options.primary[j] + '`, '
+      }
+      if (primary.length > 0) {
+        primary = ', PRIMARY KEY (' + primary.substr(0, primary.length - 2) + ')'
+      }
     }
-    if (primary.length > 0) {
-      primary = ', PRIMARY KEY (' + primary.substr(0, primary.length - 2) + ')'
-    }
-
     let SQL = 'CREATE TABLE IF NOT EXISTS ' + this.options.table + ' (' + fields + indices + primary + ')'
     SQL += ' ENGINE=' + this.options.engine
     if (this.options.partioning !== undefined) {
       SQL += ' PARTITION BY KEY (' + this.options.partioning.on + ') PARTITIONS ' + this.options.partioning.count
     }
 
-    this.mysql.query(SQL, callback)
+    this.options.mysql.query(SQL, callback)
   }
 
   this.add = function (json, callback) {
@@ -50,7 +52,7 @@ function JSON2MySQL (options) {
     }
     assignmentList = assignmentList.substr(0, assignmentList.length - 2)
     let SQL = 'INSERT ' + (this.options.ignoreInsertError ? 'IGNORE' : '') + ' INTO ' + this.options.table + ' SET ' + assignmentList
-    this.mysql.query(SQL, callback)
+    this.options.mysql.query(SQL, callback)
   }
 
   return this
--- test/test.js
+++ test/test.js
@@ -13,9 +13,17 @@ const mysql = require('mysql')
 const assert = require('assert')
 let j2m = undefined
 let config = require('./config.json')
+let correctsql = {
+  fieldsonly: 'CREATE TABLE IF NOT EXISTS simple (`id` INT NOT NULL, `timestamp` DATETIME NOT NULL, `title` VARCHAR(255) NOT NULL) ENGINE=MyISAM PARTITION BY KEY (timestamp) PARTITIONS 10',
+  fieldsindices: 'CREATE TABLE IF NOT EXISTS simple (`id` INT NOT NULL, `timestamp` DATETIME NOT NULL, `title` VARCHAR(255) NOT NULL, INDEX (`timestamp`) ) ENGINE=MyISAM PARTITION BY KEY (timestamp) PARTITIONS 10',
+  fieldsprimary: 'CREATE TABLE IF NOT EXISTS simple (`id` INT NOT NULL, `timestamp` DATETIME NOT NULL, `title` VARCHAR(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=MyISAM PARTITION BY KEY (timestamp) PARTITIONS 10',
+  fieldsindicesprimary: 'CREATE TABLE IF NOT EXISTS simple (`id` INT NOT NULL, `timestamp` DATETIME NOT NULL, `title` VARCHAR(255) NOT NULL, INDEX (`timestamp`) , PRIMARY KEY (`id`)) ENGINE=MyISAM PARTITION BY KEY (timestamp) PARTITIONS 10'
+}
 let mockDB = {
-  query: function (sql) {
-    // TODO: fake a query
+  lastQuery: '',
+  query: function (sql, callback) {
+    this.lastQuery = sql
+    callback()
   }
 }
 describe('Object Creation', function () {
@@ -58,7 +66,7 @@ describe('Object Creation', function () {
       assert.equal(j2m.options.engine, 'InnoDB')
       assert.equal(j2m.options.ignoreInsertError, false)
     } catch (e) {
-      assert.fail(e)
+      assert.fail(e.toString())
     }
   })
   it('should create an object with overriden value for engine', function () {
@@ -70,7 +78,7 @@ describe('Object Creation', function () {
       assert.equal(j2m.options.engine, newconfig.engine)
       assert.equal(j2m.options.ignoreInsertError, false)
     } catch (e) {
-      assert.fail(e)
+      assert.fail(e.toString())
     }
   })
   it('should create an object with overriden value for ignoreInsertError', function () {
@@ -82,24 +90,64 @@ describe('Object Creation', function () {
       assert.equal(j2m.options.engine, 'InnoDB')
       assert.equal(j2m.options.ignoreInsertError, newconfig.ignoreInsertError)
     } catch (e) {
-      assert.fail(e)
+      assert.fail(e.toString())
     }
   })
 })
 describe('Test-Run with MockDB', function () {
   describe('Table Setup', function () {
     beforeEach('create Object', function () {
-      config.mysql = mockDB
-      j2m = new JSON2MySQL(config)
+      let newconfig = JSON.parse(JSON.stringify(config))
+      newconfig.mysql = mockDB
+      j2m = new JSON2MySQL(newconfig)
     })
     afterEach('destroy Object', function () {
       j2m = undefined
     })
-    it('should create a table (fields only)')
-    it('should create a table (fields and indices)')
-    it('should create a table (fields and primary)')
-    it('should create a table (fields and indices and primary)')
-    it('should create a partitioned table (fields and indices and primary)')
+    it('should create a table (fields only)', function (done) {
+      delete j2m.options.indices
+      delete j2m.options.primary
+      j2m.setup(function (error) {
+        if (error) {
+          assert.fail(error.toString())
+        } else {
+          assert.equal(mockDB.lastQuery, correctsql.fieldsonly)
+          done()
+        }
+      })
+    })
+    it('should create a table (fields and indices)', function (done) {
+      delete j2m.options.primary
+      j2m.setup(function (error) {
+        if (error) {
+          assert.fail(error.toString())
+        } else {
+          assert.equal(mockDB.lastQuery, correctsql.fieldsindices)
+          done()
+        }
+      })
+    })
+    it('should create a table (fields and primary)', function (done) {
+      delete j2m.options.indices
+      j2m.setup(function (error) {
+        if (error) {
+          assert.fail(error.toString())
+        } else {
+          assert.equal(mockDB.lastQuery, correctsql.fieldsprimary)
+          done()
+        }
+      })
+    })
+    it('should create a table (fields and indices and primary)', function (done) {
+      j2m.setup(function (error) {
+        if (error) {
+          assert.fail(error.toString())
+        } else {
+          assert.equal(mockDB.lastQuery, correctsql.fieldsindicesprimary)
+          done()
+        }
+      })
+    })
     it('should fail if a incorrect datatype is given')
     it('should fail if a incorrect engine is given')
     it('should fail if a given index is not a field')
